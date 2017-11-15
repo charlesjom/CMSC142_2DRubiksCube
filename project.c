25f37e31e302ec41f03a091ea0303fcfa0f79581
elishaluzano <elishaluzano@gmail.com>
2017-11-16 00:56:14 +0800

    File IO and build cube with colors

---
+++ project.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include <string.h>
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[39m"
+
+void build_cube(char cube[6][3][3]) {
+	FILE * fp;
+	int i, j;
+
+	fp = fopen("input.txt", "r");
+
+	for(i=0; i<6; i++){
+		for(j=0; j<3; j++){
+			fscanf(fp, "%c%c%c\n", &cube[i][j][0], &cube[i][j][1], &cube[i][j][2]);
+		}
+	}
+
+	fclose(fp);
+}
+
+void printValue(char val){
+	if(val == 'R') fprintf(stdout, "%s %c%s", "\033[7m", val, "\x1B[34m" RED);
+	if(val == 'O') fprintf(stdout, "%s %c%s", "\033[7m", val, "\x1B[34m" CYN);
+	if(val == 'B') fprintf(stdout, "%s %c%s", "\033[7m", val, "\x1B[34m" BLU);
+	if(val == 'G') fprintf(stdout, "%s %c%s", "\033[7m", val, "\x1B[34m" GRN);
+	if(val == 'Y') fprintf(stdout, "%s %c%s", "\033[7m", val, "\x1B[34m" YEL);
+	if(val == 'W') fprintf(stdout, "%s %c%s", "\033[7m", val, "\x1B[34m" WHT);
+}
+
+void print_cube(char cube[6][3][3]) {
+	int i, j;
+	char x;
+
+	for(j=0; j<3; j++){
+		printf("      ");
+		printValue(cube[0][j][0]);
+		printValue(cube[0][j][1]);
+		printValue(cube[0][j][2]);
+		printf("\n");
+	}
+
+
+	for(j=0; j<3; j++){
+		for(i=1; i<5; i++){
+			printValue(cube[i][j][0]);
+			printValue(cube[i][j][1]);
+			printValue(cube[i][j][2]);
+		}
+		printf("\n");
+	}
+
+	for(j=0; j<3; j++){
+		printf("      ");
+		printValue(cube[5][j][0]);
+		printValue(cube[5][j][1]);
+		printValue(cube[5][j][2]);
+		printf("\n");
+	}
+
+	printf("\n");
+}
+
+int main() {
+	char cube[6][3][3];
+	int i, j;
+
+	build_cube(cube);
+	print_cube(cube);
+}
