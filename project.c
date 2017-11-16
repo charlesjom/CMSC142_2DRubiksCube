cce3c596d96cae1861fa5b46287fbd83c4449a96
Charles Jo Marquez <cumarquez@up.edu.ph>
2017-11-16 13:58:01 +0800

    Fixed printing of cube

--- project.c
+++ project.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <string.h>
 #define RED   "\x1B[31m"
 #define GRN   "\x1B[32m"
@@ -7,7 +7,7 @@
 #define MAG   "\x1B[35m"
 #define CYN   "\x1B[36m"
 #define WHT   "\x1B[37m"
-#define RESET "\x1B[39m"
+#define RESET "\x1B[0m"
 
 void build_cube(char cube[6][3][3]) {
 	FILE * fp;
@@ -25,12 +25,13 @@ void build_cube(char cube[6][3][3]) {
 }
 
 void printValue(char val){
-	if(val == 'R') fprintf(stdout, "%s %c%s", "\033[7m", val, "\x1B[34m" RED);
-	if(val == 'O') fprintf(stdout, "%s %c%s", "\033[7m", val, "\x1B[34m" CYN);
-	if(val == 'B') fprintf(stdout, "%s %c%s", "\033[7m", val, "\x1B[34m" BLU);
-	if(val == 'G') fprintf(stdout, "%s %c%s", "\033[7m", val, "\x1B[34m" GRN);
-	if(val == 'Y') fprintf(stdout, "%s %c%s", "\033[7m", val, "\x1B[34m" YEL);
-	if(val == 'W') fprintf(stdout, "%s %c%s", "\033[7m", val, "\x1B[34m" WHT);
+	if(val == 'R') fprintf(stdout, "%s%s %c%s ", "\033[7m", RED, val, RED);
+	if(val == 'O') fprintf(stdout, "%s%s %c%s ", "\033[7m", CYN, val, CYN);
+	if(val == 'B') fprintf(stdout, "%s%s %c%s ", "\033[7m", BLU, val, BLU);
+	if(val == 'G') fprintf(stdout, "%s%s %c%s ", "\033[7m", GRN, val, GRN);
+	if(val == 'Y') fprintf(stdout, "%s%s %c%s ", "\033[7m", YEL, val, YEL);
+	if(val == 'W') fprintf(stdout, "%s%s %c%s ", "\033[7m", WHT, val, WHT);
+	printf(RESET);
 }
 
 void print_cube(char cube[6][3][3]) {
@@ -38,13 +39,12 @@ void print_cube(char cube[6][3][3]) {
 	char x;
 
 	for(j=0; j<3; j++){
-		printf("      ");
+		printf("         ");
 		printValue(cube[0][j][0]);
 		printValue(cube[0][j][1]);
 		printValue(cube[0][j][2]);
 		printf("\n");
 	}
-
 
 	for(j=0; j<3; j++){
 		for(i=1; i<5; i++){
@@ -56,13 +56,12 @@ void print_cube(char cube[6][3][3]) {
 	}
 
 	for(j=0; j<3; j++){
-		printf("      ");
+		printf("         ");
 		printValue(cube[5][j][0]);
 		printValue(cube[5][j][1]);
 		printValue(cube[5][j][2]);
 		printf("\n");
 	}
-
 	printf("\n");
 }
 
